b4dbadd847364f8af1a9a0387c3f1d06fcfa0917
emonBimantara <shelmondbimantara@gmail.com>
2026-08-27 01:17:04 +0700

    Feat: Add Movie + WatchlistItem model (Include relations antar tabel)

--- src/routes/movieRoutes.js
+++ src/routes/movieRoutes.js
@@ -3,7 +3,7 @@ import express from "express";
 const router = express.Router()
 
 router.get("/hello", (req, res) => {
-    res.json({message: "Hello from Express"})
+    res.json({ message: "Hello from Express" })
 })
 
 export default router
