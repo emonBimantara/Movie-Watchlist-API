bfdda44f4558c0dfbc7792046438b776f4663478
emonBimantara <shelmondbimantara@gmail.com>
2026-09-14 23:41:00 +0700

    Feat: Setup logout controller

--- src/controller/authController.js
+++ src/controller/authController.js
@@ -82,4 +82,15 @@ const login = async (req, res) => {
     })
 }
 
-export { register, login }
+const logout = async (req, res) => {
+    res.cookie("jwt", "", {
+        httpOnly: true,
+        expires: new Date(0)
+    })
+    res.status(200).json({
+        status: "success",
+        message: "Logged out succesfully"
+    })
+}
+
+export { register, login, logout }
